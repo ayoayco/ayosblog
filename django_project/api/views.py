e6aeaf48a97da7bcbf8e717859a3f0ae1661736c
Ayo <ramon.aycojr@gmail.com>
2017-11-05 14:40:34 +0800

    clean code

--- django_project/api/views.py
+++ django_project/api/views.py
@@ -6,9 +6,10 @@ Author: AyoAyco
 
 from django.http import HttpResponse
 
-# API Endpoints
 def index(request):
     """ Index view
         This is just a page to show the API is up.
     """
     return HttpResponse('This is the Blog API. It is working! For API endpoints see documentation.')
+
+# API Endpoints
