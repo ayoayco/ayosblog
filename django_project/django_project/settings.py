61267329165089e33a3f979f57b7b96a7c7e7b70
Ayo <ramon.aycojr@gmail.com>
2017-11-06 10:36:32 +0800

    add templates

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print("Base Directory: ", BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
